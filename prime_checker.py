20950b78b417c49dcd3a83efcfbd55f4728ab3a1
anudhananjay <anudhananjay7@gmail.com>
2024-10-14 20:45:37 -0700

    Update prime_checker.py

--- prime_checker.py
+++ prime_checker.py
@@ -1,12 +1,12 @@
 # prime_checker.py
-def is_prime(n):
+def is_prime(n):   // defines qualities to say number prime / composite 
     if n <= 1:
-        return False
+        return False // ensures that numbers below 1 aren't recognized as prime numbers, including negative numbers. 
     for i in range(2, int(n ** 0.5) + 1):
-        if n % i == 0:
-            return False
-    return True
+        if n % i == 0: // this ensures that the number doesn't have more than two factors, which would be the number itself and 1 for prime numbers. 
+            return False // more than two factors means that it's a composite number 
+    return True 
 
 if __name__ == "__main__":
     num = int(input("Enter a number: "))
-    print(f"{num} is a prime number: {is_prime(num)}")
+    print(f"{num} is a prime number: {is_prime(num)}") // this runs the number entered through the defined prime number qualities to see whether it's a prime number. Enters output to say if prime number or not. 
