51b38d32725d93ad91d55d6b42ef61d8869552cf
Juveria-F <jufat2006@gmail.com>
2024-10-19 15:11:03 +0530

    Update factorial.py

--- factorial.py
+++ factorial.py
@@ -7,3 +7,32 @@ def factorial(n):
 if __name__ == "__main__":
     num = int(input("Enter a number: "))
     print(f"The factorial of {num} is {factorial(num)}")
+def factorial(n):
+    if n == 0 or n == 1:
+        return 1
+    return n * factorial(n - 1)
+
+def fibonacci(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fibonacci(n - 1) + fibonacci(n - 2)
+
+def is_prime(n):
+    if n < 2:
+        return False
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+def gcd(a, b):
+    while b != 0:
+        a, b = b, a % b
+    return a
+
+if __name__ == "__main__":
+    num = int(input("Enter a number: "))
+    print(f"The factorial of {num} is {factorial(num
