37e1ad5000c6b82e6920f1e4796d3660be73a94a
shaikmahmood7 <shaikmahmood6171@gmail.com>
2024-10-19 14:58:19 +0530

    Update fizzbuzz.py

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,6 +1,5 @@
-# fizzbuzz.py
-def fizzbuzz(n):
-    for i in range(1, n + 1):
+def fizzbuzz(start, end):
+    for i in range(start, end + 1):
         if i % 3 == 0 and i % 5 == 0:
             print("FizzBuzz")
         elif i % 3 == 0:
@@ -11,4 +10,6 @@ def fizzbuzz(n):
             print(i)
 
 if __name__ == "__main__":
-    fizzbuzz(20)
+    start = int(input("Enter the starting number: "))
+    end = int(input("Enter the ending number: "))
+    fizzbuzz(start, end)
