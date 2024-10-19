5b5efa18b28d4fa59967b9e5d86815da2fc2b159
Sufyan Arshi Khan <98006399+F4rozen@users.noreply.github.com>
2024-10-19 15:18:40 +0530

    Update fizzbuzz.py

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,4 +1,5 @@
 # fizzbuzz.py
+
 def fizzbuzz(n):
     for i in range(1, n + 1):
         if i % 3 == 0 and i % 5 == 0:
@@ -10,5 +11,21 @@ def fizzbuzz(n):
         else:
             print(i)
 
-if __name__ == "__main__":
-    fizzbuzz(20)
+def fizzbuzz_list(n):
+    results = []
+    for i in range(1, n + 1):
+        if i % 3 == 0 and i % 5 == 0:
+            results.append("FizzBuzz")
+        elif i % 3 == 0:
+            results.append("Fizz")
+        elif i % 5 == 0:
+            results.append("Buzz")
+        else:
+            results.append(i)
+    return results
+
+def count_fizzbuzz(n):
+    fizz_count = buzz_count = fizzbuzz_count = 0
+    for i in range(1, n + 1):
+        if i % 3 == 0 and i % 5 == 0:
+            fizzbuzz
