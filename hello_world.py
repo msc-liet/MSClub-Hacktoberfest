5745292a4ad474e76bd56cb6b8b434e3abc66d86
Wafa4663 <Wafak2731@gmail.com>
2024-10-19 15:05:20 +0530

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,6 +1,11 @@
-# hello_world.py
-def greet():
-    print("Hello, Hacktoberfest!")
+def is_prime(n):
+    if n <= 1:
+        return False
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
 
 if __name__ == "__main__":
-    greet()
+    num = int(input("Enter a number: "))
+    print(f"{num} is a prime number: {is_prime(num)}")
