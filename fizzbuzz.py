19271d8e24e959c41cc38c42bf6391cfdbdbbcf3
Wafa4663 <Wafak2731@gmail.com>
2024-10-19 15:06:23 +0530

    Update fizzbuzz.py

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,14 +1,11 @@
-# fizzbuzz.py
-def fizzbuzz(n):
-    for i in range(1, n + 1):
-        if i % 3 == 0 and i % 5 == 0:
-            print("FizzBuzz")
-        elif i % 3 == 0:
-            print("Fizz")
-        elif i % 5 == 0:
-            print("Buzz")
-        else:
-            print(i)
+def is_prime(n):
+    if n <= 1:
+        return False
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
 
 if __name__ == "__main__":
-    fizzbuzz(20)
+    num = int(input("Enter a number: "))
+    print(f"{num} is a prime number: {is_prime(num)}")
