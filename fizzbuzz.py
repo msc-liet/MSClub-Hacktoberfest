7fcd7112e6b560d1a0ffcb25bc6d0f23fcf5b7c7
Srivaishnavi Yaddanapudi <127314796+Y-Srivaishnavi@users.noreply.github.com>
2024-10-26 21:51:02 +0530

    Add type hint and docstring.

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,5 +1,13 @@
 # fizzbuzz.py
 def fizzbuzz(n):
+    """Prints the numbers from 1 to n. 
+    For multiples of 3, print "Fizz" instead of the number. 
+    For the multiples of 5, print "Buzz". 
+    For numbers which are multiples of both 3 and 5, print "FizzBuzz".   
+    
+    Args:
+      n(int): The upper limit of the range.
+    """
     for i in range(1, n + 1):
         if i % 3 == 0 and i % 5 == 0:
             print("FizzBuzz")
