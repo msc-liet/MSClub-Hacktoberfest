0b852141cc8444b23f3090abfb75fce9f905bd89
Srivaishnavi Yaddanapudi <127314796+Y-Srivaishnavi@users.noreply.github.com>
2024-10-26 21:47:28 +0530

    Add type hint, return type, and docstring.

--- palindrome_checker.py
+++ palindrome_checker.py
@@ -1,5 +1,13 @@
 # palindrome_checker.py
-def is_palindrome(s):
+def is_palindrome(s:str)->bool:
+    """Checks whether a string is a palindrome.
+
+    Args:
+        s(str): The string to be checked.
+
+    Returns:
+        Whether the string is a palindrome.
+    """
     return s == s[::-1]
 
 if __name__ == "__main__":
